a52490048d45de8e8d88ace4461a556b88c451d0
jo <jo@surikat.pro>
2017-11-19 09:24:20 +0100

    default url options

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,18 @@
 FROM node:9
 
+ARG DEFAULT_URL=http://localhost:3000/graphql
+ARG DEFAULT_WEBSOCKET_URL=ws://localhost:3000/subscriptions
+
+ENV REACT_APP_GRAPHIQL_DEFAULT_URL=$DEFAULT_URL
+ENV REACT_APP_GRAPHIQL_DEFAULT_WEBSOCKET_URL=$DEFAULT_WEBSOCKET_URL
+
 WORKDIR /app
 
 RUN git clone https://gitlab.com/kachkaev/graphiql-workspace-app.git .
 
 RUN npm install
+
+COPY src/App.js src/App.js
 
 RUN npm run build
 
---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import { GraphiQLWorkspace, AppConfig } from 'graphiql-workspace';
+
+import 'graphiql-workspace/graphiql-workspace.css';
+import 'graphiql/graphiql.css';
+
+const bootstrapOptions = {
+  defaultUrl: process.env.REACT_APP_GRAPHIQL_DEFAULT_URL,
+  defaultWebsocketUrl: process.env.REACT_APP_GRAPHIQL_DEFAULT_WEBSOCKET_URL,
+  defaultQuery: `${process.env.REACT_APP_GRAPHIQL_DEFAULT_QUERY}`.replace(/\\n/g, '\n'),
+  defaultVariables: `${process.env.REACT_APP_GRAPHIQL_DEFAULT_VARIABLES}`.replace(/\\n/g, '\n'),
+};
+const config = new AppConfig('graphiql', bootstrapOptions);
+
+class App extends Component {
+  render() {
+    return (
+      <GraphiQLWorkspace config={config} />
+    );
+  }
+}
+
+export default App;
